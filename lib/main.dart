38502b10771c15b113368da9e57a877e49c1aef4
kangcece <kangcece@shoufashi.com>
2019-07-02 15:28:07 +0800

    widget learn

--- lib/main.dart
+++ lib/main.dart
@@ -7,27 +7,116 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
 //    var wordPair = new WordPair.random();
-    return new MaterialApp(
-      title: 'Welcome to Flutter',
-      home: new Scaffold(
-        appBar: new AppBar(
-          title: new Text('Welcome to Flutter'),
-        ),
-        body: new Center(
-//          child: new Text('Hello World'),
-          child: new RandomWords()
-        ),
-      ),
-    );
+//    return new MaterialApp(
+//      title: 'Welcome to Flutter',
+//      home: new Scaffold(
+//        appBar: new AppBar(
+//          title: new Text('Welcome to Flutter'),
+//        ),
+//        body: new Center(
+////          child: new Text('Hello World'),
+//          child: new RandomWords()
+//        ),
+//      ),
+//    );
+  return new MaterialApp(
+    title: 'start up name generator',
+    theme: new ThemeData(
+      primaryColor: Colors.white,
+    ),
+    home: new RandomWords(),
+  );
   }
 }
 class RandomWordsState extends State<RandomWords>{
+  final _suggestion = <WordPair>[];
+  final _biggerFont = const TextStyle(fontSize: 18);
+  final _saved = new Set<WordPair>();
   @override
   Widget build(BuildContext context) {
-    var wordPair = new WordPair.random();
-    return new Text(wordPair.asPascalCase);
+//    var wordPair = new WordPair.random();
+
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text("startup name"),
+        actions: <Widget>[
+          new IconButton(icon: new Icon(Icons.list), onPressed: _pushSaved)
+        ],
+      ),
+      body: _buildSuggestions(),
+    );
+  }
+  Widget _buildSuggestions(){
+    return new ListView.builder(
+      padding: const EdgeInsets.all(16),
+      // 对于每个建议的单词对都会调用一次itemBuilder，然后将单词对添加到ListTile行中
+      // 在偶数行，该函数会为单词对添加一个ListTile row.
+      // 在奇数行，该函数会添加一个分割线widget，来分隔相邻的词对。
+      // 注意，在小屏幕上，分割线看起来可能比较吃力。
+      itemBuilder: (context,i){
+        if(i.isOdd) return new Divider();
+        // 语法 "i ~/ 2" 表示i除以2，但返回值是整形（向下取整），比如i为：1, 2, 3, 4, 5
+        // 时，结果为0, 1, 1, 2, 2， 这可以计算出ListView中减去分隔线后的实际单词对数量
+        final index = i ~/2;
+        if(index >= _suggestion.length){
+          // ...接着再生成10个单词对，然后添加到建议列表
+          _suggestion.addAll(generateWordPairs().take(10));
+        }
+        return _buildRow(_suggestion[index]);
+      }
+    );
   }
 
+  Widget _buildRow(WordPair pair) {
+    final alreadySaved = _saved.contains(pair);
+    return new ListTile(
+      title: new Text(
+        pair.asPascalCase,
+        style: _biggerFont,),
+      trailing: new Icon(
+        alreadySaved?Icons.favorite:Icons.favorite_border,
+        color: alreadySaved?Colors.blue:null,
+      ),
+      onTap: (){
+        setState(() {
+          if(alreadySaved){
+            _saved.remove(pair);
+          }else{
+            _saved.add(pair);
+          }
+        });
+      },
+    );
+  }
+
+
+  void _pushSaved() {
+
+    Navigator.of(context).push(
+      new MaterialPageRoute(builder: (context){
+        final tiles = _saved.map((pair){
+          return new ListTile(
+            title: new Text(
+              pair.asPascalCase,
+              style: _biggerFont,
+            ),);
+        });
+        final divided = ListTile.divideTiles(
+            context: context,
+            tiles: tiles,
+        ).toList();
+
+        return new Scaffold(
+          appBar: new AppBar(
+            title: new Text("Saved Suggestions"),
+          ),
+          body: new ListView(
+            children: divided,
+          ),
+        );
+      })
+    );
+  }
 }
 
 class RandomWords extends StatefulWidget {
@@ -36,4 +125,6 @@ class RandomWords extends StatefulWidget {
     // TODO: implement createState
     return new RandomWordsState();
   }
+
+
 }
---
+++ lib/main2.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(
+  new Center(
+    child: new Text(
+        "Hello World",
+      textDirection: TextDirection.ltr,
+      style: new TextStyle(fontSize: 30),
+    ),
+  )
+);
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return null;
+  }
+}
